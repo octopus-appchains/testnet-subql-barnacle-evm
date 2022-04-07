39b4130a28f62db3a3a0855dd2258fc97119e46c
Mad Wiki <wudifengxiang@gmail.com>
2022-04-07 22:20:49 +0800

    init

---
+++ src/handlers/frontier-evm-handlers/index.ts
@@ -0,0 +1,30 @@
+
+import { Approval, Transaction } from "../../types";
+import { BigNumber } from "ethers";
+import { FrontierEvmEvent, FrontierEvmCall } from '@subql/contract-processors/dist/frontierEvm';
+
+// Setup types from ABI
+type TransferEventArgs = [string, string, BigNumber] & { from: string; to: string; value: BigNumber; };
+type ApproveCallArgs = [string, BigNumber] & { _spender: string; _value: BigNumber; }
+
+export async function handleFrontierEvmEvent(event: FrontierEvmEvent<TransferEventArgs>): Promise<void> {
+  const transaction = new Transaction(event.transactionHash);
+
+  transaction.value = event.args.value.toBigInt();
+  transaction.from = event.args.from;
+  transaction.to = event.args.to;
+  transaction.contractAddress = event.address;
+
+  await transaction.save();
+}
+
+export async function handleFrontierEvmCall(event: FrontierEvmCall<ApproveCallArgs>): Promise<void> {
+  const approval = new Approval(event.hash);
+
+  approval.owner = event.from;
+  approval.value = event.args._value.toBigInt();
+  approval.spender = event.args._spender;
+  approval.contractAddress = event.to;
+
+  await approval.save();
+}
---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,26 @@
+import { SubstrateBlock, SubstrateExtrinsic, SubstrateEvent } from '@subql/types'
+
+import { BlockHandler } from '../handlers/block'
+import { EventHandler, ExtrinsicHandler } from '../handlers'
+import { CallHandler } from '../handlers/call'
+
+export async function handleBlock(block: SubstrateBlock): Promise<void> {
+    const handler = new BlockHandler(block)
+    await handler.save()
+}
+
+export async function handleEvent(event: SubstrateEvent): Promise<void> {
+    const handler = new EventHandler(event)
+    await handler.save()
+}
+
+export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
+    const extrinsicHandler = new ExtrinsicHandler(extrinsic);
+    await extrinsicHandler.save()
+}
+
+export {
+    handleFrontierEvmEvent,
+    handleFrontierEvmCall
+} from "../handlers/frontier-evm-handlers";
+
